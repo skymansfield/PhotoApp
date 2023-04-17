151f3f4eccfeb9012e9331cb3c06f7ab14e92cf1
Sky Mansfield <skymansfield434@gmail.com>
2023-04-17 11:22:39 -0700

    set up file structure

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connect.js";
+
+import authRoute from "./routes/authRoute.js"
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.use('/api/v1/auth', authRoute)
+
+const port = process.env.PORT || 5004;
+
+
+
+  const start = async() => {
+    await connectDB(process.env.MONGO_URL)
+    
+ app.listen(port, () => {
+  console.log(`running on ${port}`);
+});
+  }
+
+  start()
+    
+  
+
