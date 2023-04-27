aee6a751494f113eae78b7d643dc703e8d47e76f
skymansfield <skymansfield434@gmail.com>
2023-04-27 11:06:50 -0700

    BLMS-1533

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Jumbutron from '../components/Jumbutron';
 import SearchField from '../components/SearchField';
+import Images from '../components/Images';
 
 const Dashboard = () => {
     return (
+    <>
        <Jumbutron>
         <SearchField/>
        </Jumbutron>
-
+        <Images />
+        </>
    
     );
 }
