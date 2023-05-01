08e285721dbb8efdac23a0a12e5653d88d6955c9
skymansfield <skymansfield434@gmail.com>
2023-04-30 18:43:04 -0700

    BLMS-1534

--- client/src/components/Skeleton.js
+++ client/src/components/Skeleton.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Skeleton = () => {
-    return (
-        <div className='animate-pulse' >
-            <div className='bg-gray-300 rounded-lg h-72' >
-
-            </div>
-
-        </div>
-    );
-}
+const Skeleton = ({ item }) => {
+  return [...Array(item).keys()].map(() => (
+    <div className="animate-pulse">
+      <div className="bg-gray-300 rounded-lg h-72"></div>
+    </div>
+  ));
+};
 
 export default Skeleton;
