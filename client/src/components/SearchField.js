21d898df7eb8527a94ccb684eefbc825385ced75
skymansfield <skymansfield434@gmail.com>
2023-04-30 18:52:50 -0700

    BLMS-1546

--- client/src/components/SearchField.js
+++ client/src/components/SearchField.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useContext, useState } from "react";
 import { ImageContext } from "../pages/Dashboard.js";
 
@@ -29,18 +28,12 @@ const SearchField = () => {
     }
   };
 
-=======
-import React from "react";
-
-const SearchField = () => {
->>>>>>> da56636f1a512be0ee8f54035226fa2fe3da29e5
   return (
     <div className="flex items-center">
       <input
         type="search"
         className="bg-gray-50 border border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
         placeholder="Search Anything..."
-<<<<<<< HEAD
         value={searchValue}
         onChange={handleInputChange}
         onKeyDown={handleEnterSearch}
@@ -50,10 +43,6 @@ const SearchField = () => {
         disabled={!searchValue}
         className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2  focus:ring-blue-300 disabled:bg-gray-400"
       >
-=======
-      />
-      <button className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2  focus:ring-blue-300 disabled:bg-gray-400">
->>>>>>> da56636f1a512be0ee8f54035226fa2fe3da29e5
         Search
       </button>
     </div>
